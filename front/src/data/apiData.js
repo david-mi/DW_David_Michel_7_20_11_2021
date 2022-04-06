f8027081d0e3978d338a00087fd152b657cbeb2e
David Michel <david.michel2809@gmail.com>
2022-04-06 15:19:26 +0200

    FRONT | ajout des api distantes

--- front/src/data/apiData.js
+++ front/src/data/apiData.js
@@ -1,13 +1,33 @@
 /* stockage de toutes les infos liées à l'api ainsi qu'une fonction 
 qui nous retourne des headers qu'on utiliser pour les requêtes axios */
 
-export const apiUser = 'http://localhost:3000/api/auth/users';
-export const apiMessage = 'http://localhost:3000/api/messages';
-export const apiComment = 'http://localhost:3000/api/comments';
-export const apiLogin = 'http://localhost:3000/api/auth/login';
-export const apiSignup = 'http://localhost:3000/api/auth/signup';
-export const apiModeration = 'http://localhost:3000/api/mod';
-export const apiAdmin = 'http://localhost:3000/api/mod/admin';
+export let apiUser = '';
+export let apiMessage = '';
+export let apiComment = '';
+export let apiLogin = '';
+export let apiSignup = '';
+export let apiModeration = '';
+export let apiAdmin = '';
+
+if (window.location.hostname !== "localhost") {
+  apiUser = 'https://david-mi-groupomania-frontend.herokuapp.com/api/auth/users';
+  apiMessage = 'https://david-mi-groupomania-frontend.herokuapp.com/api/messages';
+  apiComment = 'https://david-mi-groupomania-frontend.herokuapp.com/api/comments';
+  apiLogin = 'https://david-mi-groupomania-frontend.herokuapp.com/api/auth/login';
+  apiSignup = 'https://david-mi-groupomania-frontend.herokuapp.com/api/auth/signup';
+  apiModeration = 'https://david-mi-groupomania-frontend.herokuapp.com/api/mod';
+  apiAdmin = 'https://david-mi-groupomania-frontend.herokuapp.com/api/mod/admin';
+}
+else {
+
+  apiUser = 'http://localhost:3000/api/auth/users';
+  apiMessage = 'http://localhost:3000/api/messages';
+  apiComment = 'http://localhost:3000/api/comments';
+  apiLogin = 'http://localhost:3000/api/auth/login';
+  apiSignup = 'http://localhost:3000/api/auth/signup';
+  apiModeration = 'http://localhost:3000/api/mod';
+  apiAdmin = 'http://localhost:3000/api/mod/admin';
+}
 
 export const getHeaders = (token, option) => {
 
